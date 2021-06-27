97a461a7c21d6a9a1739db0f7cc1587e9e06af61
He-Feng Hsieh <x.geometric@gmail.com>
2021-06-28 00:26:41 +0800

    add Post Bounce test problem

--- include/Typedef.h
+++ include/Typedef.h
@@ -53,6 +53,7 @@ const TestProbID_t
    TESTPROB_HYDRO_BARRED_POT                   =   51,
 
    TESTPROB_HYDRO_GREP_MIGRATIONTEST           =   20,
+   TESTPROB_HYDRO_CCSN_POSTBOUNCE              =   21,
 
    TESTPROB_ELBDM_EXTPOT                       = 1000;
 
--- src/Init/Init_TestProb.cpp
+++ src/Init/Init_TestProb.cpp
@@ -23,6 +23,7 @@ void Init_TestProb_Hydro_JeansInstability();
 void Init_TestProb_Hydro_BarredPot();
 
 void Init_TestProb_Hydro_GREP_MigrationTest();
+void Init_TestProb_Hydro_CCSN_PostBounce();
 
 void Init_TestProb_ELBDM_ExtPot();
 
@@ -69,6 +70,7 @@ void Init_TestProb()
       case TESTPROB_HYDRO_JEANS_INSTABILITY :            Init_TestProb_Hydro_JeansInstability();            break;
 
       case TESTPROB_HYDRO_GREP_MIGRATIONTEST :           Init_TestProb_Hydro_GREP_MigrationTest();          break;
+      case TESTPROB_HYDRO_CCSN_POSTBOUNCE :              Init_TestProb_Hydro_CCSN_PostBounce();             break;
 
       case TESTPROB_ELBDM_EXTPOT :                       Init_TestProb_ELBDM_ExtPot();                      break;
 
