b8e358b1a3cd870f15e62c43a27d0f8fa78cdcae
Inhyeok-Song <ihsong@gapp.nthu.edu.tw>
2021-09-10 21:29:51 +0800

    check indices

--- src/EoS/Nuclear/findtoreps.cpp
+++ src/EoS/Nuclear/findtoreps.cpp
@@ -112,10 +112,14 @@ void findtoreps( const real x, const real y, const real z,
       iy = (int)( (y - yt[0] )*dyi );
       iz = (int)( (z - zt[0] )*dzi );
 
+      ix = MAX( 1, MIN( ix, nx-2 ) );
+      iy = MAX( 1, MIN( iy, ny-2 ) );
+      iz = MAX( 1, MIN( iz, nz-2 ) );
+
 
       // linear interpolation at boundaries
       if ( ix == 0    || iy == 0    || iz == 0 ||
-           ix >= nx-2 || iy >= ny-2 || iz >= nz-2 )
+           ix == nx-2 || iy == ny-2 || iz == nz-2 )
       {
          findtoreps_bdry( x, y, z, found_ltoreps, alltables_mode, nx, ny, nz, ntoreps,
                           xt, yt, zt, logtoreps, keymode, keyerr );
