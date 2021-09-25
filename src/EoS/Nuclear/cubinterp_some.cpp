3e59494fadeec51ffd5c9bbb926fc42c95512e94
He-Feng Hsieh <x.geometric@gmail.com>
2021-09-25 22:47:11 +0800

    Optimize linear and cubic interpolation schemes

--- src/EoS/Nuclear/cubinterp_some.cpp
+++ src/EoS/Nuclear/cubinterp_some.cpp
@@ -37,7 +37,8 @@ void nuc_eos_C_linterp_some( const real x, const real y, const real z,
 //
 // Parameter   :  x           : Input vector of first  variable (rho)
 //                y           : Input vector of second variable (temp/eps)
-//                z           : Input vector of third  variable (Y_e)
+//                z           : Input vector of third  variable (Ye)
+//                TargetIdx   : Indices of thermodynamic variables to be returned
 //                output_vars : Output variables of interpolated function values
 //                alltables   : 3D array of tabulated variables
 //                nx          : X-dimension of table
@@ -62,9 +63,8 @@ void nuc_eos_C_cubinterp_some( const real x, const real y, const real z,
    real dx, dy, dz, dxi, dyi, dzi;
    real delx, dely, delz;
    real u[4], v[4], w[4];
-   real r[4], q[4];
-   real vox;
-   int  ix, iy, iz;
+   real r, q, vox;
+   int  ix, iy, iz, iv;
    int  nxy, nxyz;
 
    nxy  = nx*ny;
@@ -72,7 +72,6 @@ void nuc_eos_C_cubinterp_some( const real x, const real y, const real z,
 
 
 // determine spacing parameters of equidistant (!!!) table
-#  if 1
    dx  = ( xt[nx-1] - xt[0] ) / (real)(nx-1);
    dy  = ( yt[ny-1] - yt[0] ) / (real)(ny-1);
    dz  = ( zt[nz-1] - zt[0] ) / (real)(nz-1);
@@ -80,17 +79,6 @@ void nuc_eos_C_cubinterp_some( const real x, const real y, const real z,
    dxi = (real)1.0 / dx;
    dyi = (real)1.0 / dy;
    dzi = (real)1.0 / dz;
-#  endif
-
-#  if 0
-   dx  = drho;
-   dy  = deps;
-   dz  = dye;
-
-   dxi = drhoi;
-   dyi = depsi;
-   dzi = dyei;
-#  endif
 
 
 // determine location in table
@@ -100,8 +88,8 @@ void nuc_eos_C_cubinterp_some( const real x, const real y, const real z,
 
 
 // linear interpolation at boundaries
-   if ( ix == 0  ||  iy == 0  ||  iz == 0  ||
-        ix >= nx-2  ||  iy >= ny-2  ||  iz >= nz-2 )
+   if ( ix ==    0  ||  iy ==    0  ||  iz ==    0  ||
+        ix >= nx-2  ||  iy >= ny-2  ||  iz >= nz-2     )
    {
       nuc_eos_C_linterp_some( x, y, z, TargetIdx, output_vars, alltables,
                               nx, ny, nz, nvars, xt, yt, zt );
@@ -138,32 +126,32 @@ void nuc_eos_C_cubinterp_some( const real x, const real y, const real z,
    w[2] = (real)-1.5*delz3 + (real)2.0*delz2 + (real)0.5*delz;
    w[3] = (real) 0.5*delz3 - (real)0.5*delz2;
 
+
    for (int idx=0; idx<nvars; idx++)
    {
-      int iv = TargetIdx[idx];
-
+      iv  = TargetIdx[idx] * nxyz;
+      pv  = alltables + iv + (ix-1) + (iy-1)*nx + (iz-1)*nxy;
       vox = (real)0.0;
-      pv  = alltables + (ix-1) + (iy-1)*nx + (iz-1)*nxy + iv*nxyz;
 
       for (int k=0; k<4; k++)
       {
-         q[k] = (real)0.0;
+         q = (real)0.0;
 
          for (int j=0; j<4; j++)
          {
-            r[j] = (real)0.0;
+            r = (real)0.0;
 
             for (int i=0; i<4; i++)
             {
-               r[j] += u[i]* *pv;
-               pv   += 1;
+               r  += (*pv)*u[i];
+               pv += 1;
             }
 
-            q[k] += v[j]*r[j];
-            pv   += nx - 4;
+            q  += r*v[j];
+            pv += nx - 4;
          }
 
-         vox += w[k]*q[k];
+         vox += q*w[k];
          pv  += nxy - 4*nx;
       } // for (int k=0; k<4; k++)
 
@@ -171,16 +159,10 @@ void nuc_eos_C_cubinterp_some( const real x, const real y, const real z,
    } // for (int idx=0; idx<nvars; idx++)
 
 
-// linear interpolation at boundaries (where output_vars[0] will be NaN)
+// linear interpolation if cubic interpolation fails
    if ( output_vars[0] != output_vars[0] )
-   {
       nuc_eos_C_linterp_some( x, y, z, TargetIdx, output_vars, alltables,
                               nx, ny, nz, nvars, xt, yt, zt );
-      return;
-   }
-
-
-   return;
 
 } // FUNCTION : nuc_eos_C_cubinterp_some
 
--- src/EoS/Nuclear/linterp_some.cpp
+++ src/EoS/Nuclear/linterp_some.cpp
@@ -24,7 +24,8 @@ void nuc_eos_C_linterp_some( const real x, const real y, const real z,
 //
 // Parameter   :  x           : Input vector of first  variable (rho)
 //                y           : Input vector of second variable (temp/eps)
-//                z           : Input vector of third  variable (Y_e)
+//                z           : Input vector of third  variable (Ye)
+//                TargetIdx   : Indices of thermodynamic variables to be returned
 //                output_vars : Output variables of interpolated function values
 //                alltables   : 3D array of tabulated variables
 //                nx          : X-dimension of table
@@ -45,32 +46,20 @@ void nuc_eos_C_linterp_some( const real x, const real y, const real z,
 {
 
 // helper variables
-   real fh[8], delx, dely, delz, a[8];
+   real delx, dely, delz, a[8], fh[8];
    real dx, dy, dz, dxi, dyi, dzi, dxyi, dxzi, dyzi, dxyzi;
-   int  ix, iy, iz;
+   int  ix, iy, iz, iv, idx[8];
    int  nxyz = nx*ny*nz;
 
 
 // determine spacing parameters of equidistant (!!!) table
-#  if 1
-   dx  = ( xt[nx-1] - xt[0] ) / (real)(nx-1);
-   dy  = ( yt[ny-1] - yt[0] ) / (real)(ny-1);
-   dz  = ( zt[nz-1] - zt[0] ) / (real)(nz-1);
+   dx    = ( xt[nx-1] - xt[0] ) / (real)(nx-1);
+   dy    = ( yt[ny-1] - yt[0] ) / (real)(ny-1);
+   dz    = ( zt[nz-1] - zt[0] ) / (real)(nz-1);
 
-   dxi = (real)1.0 / dx;
-   dyi = (real)1.0 / dy;
-   dzi = (real)1.0 / dz;
-#  endif
-
-#  if 0
-   dx  = drho;
-   dy  = deps;
-   dz  = dye;
-
-   dxi = drhoi;
-   dyi = depsi;
-   dzi = dyei;
-#  endif
+   dxi   = (real)1.0 / dx;
+   dyi   = (real)1.0 / dy;
+   dzi   = (real)1.0 / dz;
 
    dxyi  = dxi*dyi;
    dxzi  = dxi*dzi;
@@ -83,9 +72,9 @@ void nuc_eos_C_linterp_some( const real x, const real y, const real z,
    iy = 1 + (int)( ( y - yt[0] )*dyi );
    iz = 1 + (int)( ( z - zt[0] )*dzi );
 
-   ix = MAX( 1, MIN( ix, nx-1 ) );
-   iy = MAX( 1, MIN( iy, ny-1 ) );
-   iz = MAX( 1, MIN( iz, nz-1 ) );
+   ix = MAX(  1, MIN( ix, nx-1 )  );
+   iy = MAX(  1, MIN( iy, ny-1 )  );
+   iz = MAX(  1, MIN( iz, nz-1 )  );
 
 
 // set up aux vars for interpolation
@@ -93,21 +82,19 @@ void nuc_eos_C_linterp_some( const real x, const real y, const real z,
    dely = yt[iy] - y;
    delz = zt[iz] - z;
 
-   int idx[8];
-
-   idx[0] = (ix  ) + nx*(  (iy  ) + ny*(iz  )  );
-   idx[1] = (ix-1) + nx*(  (iy  ) + ny*(iz  )  );
-   idx[2] = (ix  ) + nx*(  (iy-1) + ny*(iz  )  );
-   idx[3] = (ix  ) + nx*(  (iy  ) + ny*(iz-1)  );
-   idx[4] = (ix-1) + nx*(  (iy-1) + ny*(iz  )  );
-   idx[5] = (ix-1) + nx*(  (iy  ) + ny*(iz-1)  );
-   idx[6] = (ix  ) + nx*(  (iy-1) + ny*(iz-1)  );
-   idx[7] = (ix-1) + nx*(  (iy-1) + ny*(iz-1)  );
+   idx[0] = ix + nx*( iy + ny*iz ); // ( ix,   iy,   iz   )
+   idx[1] = idx[0] - 1;             // ( ix-1, iy,   iz   )
+   idx[2] = idx[0] - nx;            // ( ix,   iy-1, iz   )
+   idx[3] = idx[0] - nx*ny;         // ( ix,   iy,   iz-1 )
+   idx[4] = idx[2] - 1;             // ( ix-1, iy-1, iz   )
+   idx[5] = idx[3] - 1;             // ( ix-1, iy,   iz-1 )
+   idx[6] = idx[3] - nx;            // ( ix,   iy-1, iz-1 )
+   idx[7] = idx[6] - 1;             // ( ix-1, iy-1, iz-1 )
 
 
    for (int i=0; i<nvars; i++)
    {
-      int iv = TargetIdx[i] * nxyz;
+      iv = TargetIdx[i] * nxyz;
 
 //    set up aux vars for interpolation assuming array ordering (iv, ix, iy, iz)
       fh[0] = alltables[ iv + idx[0] ];
@@ -139,9 +126,6 @@ void nuc_eos_C_linterp_some( const real x, const real y, const real z,
                      + a[6]*dely*delz
                      + a[7]*delx*dely*delz;
    } // for (int i=0; i<nvars; i++)
-
-
-   return;
 
 } // FUNCTION : nuc_eos_C_linterp_some
 
