8c31533463a2c6d6b5af546b83c370ec98e5f98d
Inhyeok-Song <ihsong@gapp.nthu.edu.tw>
2022-09-06 13:52:36 +0800

    change SRC_NAUX_DLEP

--- include/Macro.h
+++ include/Macro.h
@@ -748,7 +748,7 @@
 #endif
 
 #if ( MODEL == HYDRO )
-#  define SRC_NAUX_DLEP          9     // SrcTerms.Dlep_AuxArray_Flt/Int[]
+#  define SRC_NAUX_DLEP          8     // SrcTerms.Dlep_AuxArray_Flt/Int[]
 #  define SRC_DLEP_PROF_NVAR     6     // SrcTerms.Dlep_Profile_DataDevPtr[]/RadiusDevPtr[]
 #  define SRC_DLEP_PROF_NBINMAX  4000
 #  define SRC_NAUX_LIGHTBULB     2     // SrcTerms.Lightbulb_AuxArray_Flt/Int[]
