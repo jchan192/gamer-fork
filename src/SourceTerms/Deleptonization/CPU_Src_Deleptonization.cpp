f90e6bf12e6517663aab482fde5fa44177aab117
Inhyeok-Song <ihsong@gapp.nthu.edu.tw>
2022-09-13 21:26:08 +0800

    corrected bug in yeofrhofunc

--- src/SourceTerms/Deleptonization/CPU_Src_Deleptonization.cpp
+++ src/SourceTerms/Deleptonization/CPU_Src_Deleptonization.cpp
@@ -487,7 +487,7 @@ real yeofrhofunc( const real Dens_CGS, const real Delep_Rho1, const real Delep_R
 
    real XofRho, Ye;
 
-   XofRho = (real)2.0 * LOG10( Dens_CGS / Delep_Rho2 / Delep_Rho1 )
+   XofRho = LOG10( SQR( Dens_CGS ) / Delep_Rho2 / Delep_Rho1 )
           / LOG10( Delep_Rho2 / Delep_Rho1 );
    XofRho = MAX( (real)-1.0, MIN( (real)1.0, XofRho ) );
 
