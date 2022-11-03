9b349a1adcfd3a278ddf43b7290e53858f141912
Inhyeok-Song <ihsong@gapp.nthu.edu.tw>
2022-11-03 08:27:30 +0800

    correct the checking OPT__RESET_FLUID

--- src/Auxiliary/Aux_Check_Parameter.cpp
+++ src/Auxiliary/Aux_Check_Parameter.cpp
@@ -739,8 +739,9 @@ void Aux_Check_Parameter()
 #        error : ERROR : must enable NUC_TABLE_MODE for EOS_NUCLEAR !!
 #     elif ( NUC_TABLE_MODE != NUC_TABLE_MODE_TEMP  &&  NUC_TABLE_MODE != NUC_TABLE_MODE_ENGY )
 #        error : ERROR : unsupported nuclear EoS table type (NUC_TABLE_MODE_TEMP/NUC_TABLE_MODE_ENGY) !!
-#     elif ( NUC_TABLE_MODE == NUC_TABLE_MODE_TEMP  &&  !OPT__RESET_FLUID )
-#        error : ERROR : must enable OPT__RESET_FLUID for nuclear EoS table type NUC_TABLE_MODE_TEMP !!
+#     elif ( NUC_TABLE_MODE == NUC_TABLE_MODE_TEMP )
+      if ( !OPT__RESET_FLUID )
+         Aux_Error( ERROR_INFO, "ERROR : must enable OPT__RESET_FLUID for nucelar EoS table type NUC_TABLE_MODE_TEMP !!\n" );
 #     endif
 #  endif // if ( EOS != EOS_NUCLEAR )
 
