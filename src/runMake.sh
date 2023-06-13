a6842d739ddd8e1a9a1e8d2a3554af2b03e2c2af
jchan192 <95039916+jchan192@users.noreply.github.com>
2023-06-13 14:43:25 +0800

    Delete runMake.sh

--- src/runMake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash                                                                                                                                                                                                                                              
-set -e 
-make -j 
-cp gamer ../../gamer-cudft/bin/Gravity
-cd ../../gamer-cudft/bin/Gravity
-./gamer > log  
-cd ../../../gamer-fork/src    
-
